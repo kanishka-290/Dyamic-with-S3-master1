42a23b08e5120bbdf1b37a00ba1612348293a1d4
Kanishka Shakya <68329914+kanishka-290@users.noreply.github.com>
2023-05-09 15:18:01 +0530

    finalcommit

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,7 +2,7 @@ import React, {Component, Fragment} from 'react';
 import Header from './Header';
 import RestaurantsDisplay from './Restaurants';
 
-const url = "http://3.110.85.198:8443/restaurant/";
+const url = "http://35.154.1.81:8443/restaurant/";
 
 class Home extends Component{
     constructor(){
